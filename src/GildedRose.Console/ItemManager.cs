30145820e9351aed955a5633a2c4be42e66aeb08
Karim Ljung <karim.ljung@gmail.com>
2017-07-12 15:58:37 +0200

    Refactoring if-statements handling the increase and decrease of the items quality. Removing obsolete if-statements for checking quality - it is now handled when calling the increase and decrease methods for an item.

--- src/GildedRose.Console/ItemManager.cs
+++ src/GildedRose.Console/ItemManager.cs
@@ -41,49 +41,29 @@ namespace GildedRose.Console
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                // Decrease an item quality if it is not aged brie or Backstage passses.
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-
-                    // Decrease quality for all items except Sulfuras.
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    DecreaseItemQuality(Items[i]);
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    IncreaseItemQuality(Items[i]);
+
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].SellIn < 11)
                         {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                            IncreaseItemQuality(Items[i]);
+                        }
 
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                        if (Items[i].SellIn < 6)
+                        {
+                            IncreaseItemQuality(Items[i]);
                         }
                     }
                 }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+                UpdateSellInForItem(Items[i]);
 
                 if (Items[i].SellIn < 0)
                 {
@@ -106,12 +86,36 @@ namespace GildedRose.Console
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        IncreaseItemQuality(Items[i]);
                     }
                 }
+            }
+        }
+
+        private void DecreaseItemQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality -= 1;
+                }
+            }
+        }
+
+        private void IncreaseItemQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+        }
+
+        private void UpdateSellInForItem(Item item)
+        {
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn -= 1;
             }
         }
     }

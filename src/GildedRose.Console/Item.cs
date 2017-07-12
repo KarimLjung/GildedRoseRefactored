7f2f41a147116d61b27537713aa5f9ae915ca863
Karim Ljung <karim.ljung@gmail.com>
2017-07-12 17:24:25 +0200

    Adding documentation to the Item and ItemManager class. Including intellisense information about the new classes.

--- src/GildedRose.Console/Item.cs
+++ src/GildedRose.Console/Item.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace GildedRose.Console
 {
+    /// <summary>
+    /// An item has a name, sellin value (the number of days to sell the item),
+    /// and a given quality.
+    /// </summary>
     public class Item
     {
         public string Name { get; set; }
--- src/GildedRose.Console/ItemManager.cs
+++ src/GildedRose.Console/ItemManager.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace GildedRose.Console
 {
+    /// <summary>
+    /// The item manager is responsible for updating the quality and sellin values
+    /// for all items.
+    /// </summary>
     public class ItemManager
     {
         private IList<Item> items;
@@ -33,10 +37,21 @@ namespace GildedRose.Console
                                           };
         }
 
+        /// <summary>
+        /// Returns a list of items.
+        /// </summary>
+        /// <returns>A list of items</returns>
         public IList<Item> GetItems()
         {
             return items;
         }
+
+        /// <summary>
+        /// Updates the quality of an item. Depending on the item type,
+        /// the item's quality and decrease or increase.
+        /// All items except Backstage passes and Aged Brie decreases
+        /// by 2 once the SellIn is less than zero.
+        /// </summary>
         public void UpdateQuality()
         {
 
@@ -64,17 +79,22 @@ namespace GildedRose.Console
                         }
                     }
                 }
-                DecrementSellInForItem(item);
+                DecrementItemSellIn(item);
 
                 // In this case aged brie increases in quality but all other items do not.
                 if (IsItemBelowSellIn(item))
                 {
-                    UpdateItemAfterSellIn(item);
+                    UpdateItemQualityAfterSellIn(item);
                 }
             }
         }
 
-        private void UpdateItemAfterSellIn(Item item)
+        /// <summary>
+        /// Increases or decreases an item faster than before
+        /// because the SellIn value is less than zero.
+        /// </summary>
+        /// <param name="item"></param>
+        private void UpdateItemQualityAfterSellIn(Item item)
         {
             if (item.SellIn < 0)
             {
@@ -95,16 +115,29 @@ namespace GildedRose.Console
             }
         }
 
+        /// <summary>
+        /// Checks if an item's SellIn value is below zero.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>True if an item's SellIn value is below zero.</returns>
         private bool IsItemBelowSellIn(Item item)
         {
             return (item.SellIn < 0 ? true : false);
         }
 
+        /// <summary>
+        /// Sets the item quality to zero.
+        /// </summary>
+        /// <param name="item">The item to clear.</param>
         private void ClearItemQuality(Item item)
         {
             item.Quality = 0;
         }
 
+        /// <summary>
+        /// Decrements the quality of an item by one.
+        /// </summary>
+        /// <param name="item">The item to decrement..</param>
         private void DecrementItemQuality(Item item)
         {
             if (item.Quality > 0)
@@ -116,6 +149,10 @@ namespace GildedRose.Console
             }
         }
 
+        /// <summary>
+        /// Increments the item quality.
+        /// </summary>
+        /// <param name="item">The item to increment.</param>
         private void IncrementItemQuality(Item item)
         {
             if (item.Quality < 50)
@@ -124,7 +161,11 @@ namespace GildedRose.Console
             }
         }
 
-        private void DecrementSellInForItem(Item item)
+        /// <summary>
+        /// Decrements the SellIn value of an item.
+        /// </summary>
+        /// <param name="item">The item to decrement.</param>
+        private void DecrementItemSellIn(Item item)
         {
             if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
